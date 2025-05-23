8a02582292cac5d17a016118dedc70a36017233e
Thejas A <thejas2246@gmail.com>
2025-05-23 10:53:31 +0530

    finish the project

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const container = document.createElement('div');
 container.setAttribute("class","container");
 document.body.appendChild(container);
 
-let gridSize = 25;
+let gridSize = 16;
 
 createGrid(gridSize);
 
@@ -16,14 +16,50 @@ function createGrid(gSize){
     children.forEach((item)=>{
         item.style.flexBasis = `${(800-gSize)/gSize}px`;
     })
+    hoverEvent();
 }
 
-const children = document.querySelectorAll(".child");
-children.forEach((item)=>{
-  
-    item.addEventListener('mouseover',changeHoverColor)
+function hoverEvent(){
+    const children = document.querySelectorAll(".child");
+    children.forEach((item)=>{
+        let opacity = 0;
+        item.addEventListener('mouseover',(event)=>{
+            opacity= changeHoverColor(event,opacity)
+        })
+    })
+}
+
+
+function changeHoverColor(item,opacity){
+    opacity = opacity + 0.1;
+    let r = generateRandomNumber();
+    let g = generateRandomNumber();
+    let b = generateRandomNumber();
+    item.target.style.backgroundColor = `rgba(${r},${g},${b},${1 - opacity})`;
+    return opacity;
+}
+
+function generateRandomNumber(){
+    let randomNumber = Math.floor(Math.random()*255);
+    return randomNumber;
+}
+function removeGrid(){
+    const children = document.querySelectorAll('.child');
+    children.forEach((item)=>{
+        container.removeChild(item);
+    })
+}
+const button = document.querySelector("button")
+button.addEventListener('click',()=>{
+    let gridSize=parseInt(prompt("Enter a number"));
+    if(gridSize>100||isNaN(gridSize)){
+        alert("invalid number");
+    }
+    else{
+        removeGrid();
+        createGrid(gridSize)
+        hoverEvent();
+    }
+
 })
 
-function changeHoverColor(item){
-    item.target.style.backgroundColor = "red";
-}
